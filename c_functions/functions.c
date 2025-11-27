d773afb361d43c1878a97c2f2b7e0d2844a5449e
Ali <129314794+0TRUTHER0@users.noreply.github.com>
2025-11-27 21:21:08 +0100

    Add files via upload

---
+++ c_functions/functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * returns string length
+ */
+int length(char name[]){
+    int i=0;
+    while(name[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
+char name[] = "dimden";
+
+int main(){
+
+
+  return 0;
+}
